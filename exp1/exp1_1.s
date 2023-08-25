f1b8a13351506b9317a5f8ff5ce48fa59d8dca75
Nippun Kumaar <nippun05@gmail.com>
2023-08-25 10:05:28 +0530

    lab-1 commit

---
+++ exp1/exp1_1.s
@@ -0,0 +1,8 @@
+ AREA exp1_1, CODE, READONLY
+
+ mov r1, #10
+ mov r2, r1
+ mvn r3, r1
+e b e
+
+ end
---
+++ exp1/exp1_2.s
@@ -0,0 +1,10 @@
+ AREA exp1_2, CODE, READONLY
+
+ mov r2, #0x12
+ mov r3, #0x13
+ add r1, r2, r3
+ sub r4, r2, r3
+ and r5, r3, r2
+
+e b e
+ end
