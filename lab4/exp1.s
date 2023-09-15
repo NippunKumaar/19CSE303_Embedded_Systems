17e15dc966c2aa3ac40cc209fc2e8fb274c87b9e
Nippun Kumaar <nippun05@gmail.com>
2023-09-15 09:40:05 +0530

    changed ldrsh line and added comments

--- lab4/exp1.s
+++ lab4/exp1.s
@@ -11,9 +11,9 @@ result dcw 0
  ldr r3, =result
  mov r4, #0			;intializing the result register
  ldr r5, [r2]		;loading len
-loading ldrsh r6, [r1]		;loading a byte for addition
- movs r6, r6
- addmi r4, r4, #1
+loading ldr r6, [r1]		;loading a byte for addition
+ movs r6, r6				; setting flags
+ addmi r4, r4, #1			; Checking N-flag
  add r1, r1, #0x04	; incrementing the address to point to next byte
  sub r5, r5, #0x01	; decrementing length by 1
  cmp r5, #0x00		; checking for length to be zero
